37556800731b7ab07941fb3e4ed526e4b471039c
Espen Trydal <espen@trydal.io>
2022-03-15 23:41:42 +0100

    test for elisp directories

--- init.el
+++ init.el
@@ -1,17 +1,19 @@
 ;; init.el
-(add-to-list 'load-path "h:/33-programmer/emacs.d")
+(add-to-list 'load-path "~/33-programmer/emacs.d")
 
 (cond ((and (= emacs-major-version 22)
             (> emacs-minor-version 2))
        ;; Emacs version 22.3
-       (add-to-list 'load-path "h:/33-programmer/emacs.d/elisp-22")
        (let ((default-directory "h:/33-programmer/emacs.d/elisp-22"))
-         (normal-top-level-add-subdirs-to-load-path))
+	 (if (file-directory-p default-directory)
+             ((add-to-list 'load-path "h:/33-programmer/emacs.d/elisp-22")
+	      (normal-top-level-add-subdirs-to-load-path))))
        (require 'config-22))
       (t
        ;; Emacs version 23 or later
-       (add-to-list 'load-path "h:/33-programmer/emacs.d/elisp")
-       (let ((default-directory "h:/33-programmer/emacs.d/elisp"))
-         (normal-top-level-add-subdirs-to-load-path))
+       (let ((default-directory "~/33-programmer/emacs.d/elisp"))
+	 (if (file-directory-p default-directory)
+             ((add-to-list 'load-path "~/33-programmer/emacs.d/elisp")
+	      (normal-top-level-add-subdirs-to-load-path))))
        (require 'config)))
 
