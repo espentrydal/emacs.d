e518b50dd9952d9fc83e2959addc961c9c764dff
Espen Trydal <espen@trydal.io>
2022-03-17 16:51:32 +0100

    Fix elisp code

--- init.el
+++ init.el
@@ -5,15 +5,15 @@
             (> emacs-minor-version 2))
        ;; Emacs version 22.3
        (let ((default-directory "h:/33-programmer/emacs.d/elisp-22"))
-	 (if (file-directory-p default-directory)
-             ((add-to-list 'load-path "h:/33-programmer/emacs.d/elisp-22")
-	      (normal-top-level-add-subdirs-to-load-path))))
+	     (when (file-directory-p default-directory)
+           (add-to-list 'load-path default-directory)
+	       (normal-top-level-add-subdirs-to-load-path)))
        (require 'config-22))
       (t
        ;; Emacs version 23 or later
        (let ((default-directory "~/33-programmer/emacs.d/elisp"))
-	 (if (file-directory-p default-directory)
-             ((add-to-list 'load-path "~/33-programmer/emacs.d/elisp")
-	      (normal-top-level-add-subdirs-to-load-path))))
+	     (when (file-directory-p default-directory)
+           (add-to-list 'load-path default-directory)
+           (normal-top-level-add-subdirs-to-load-path)))
        (require 'config)))
 
