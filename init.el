99bbaa47d53da32352f11f9584c3a557bc9486f4
Espen Trydal <espen@trydal.io>
2022-04-01 13:57:06 +0200

    h:

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; init.el
-(add-to-list 'load-path "~/33-programmer/emacs.d")
+(add-to-list 'load-path "h:/33-programmer/emacs.d")
 
 (cond ((and (= emacs-major-version 22)
             (> emacs-minor-version 2))
@@ -11,7 +11,7 @@
        (require 'config-22))
       (t
        ;; Emacs version 23 or later
-       (let ((default-directory "~/33-programmer/emacs.d/elisp"))
+       (let ((default-directory "h:/33-programmer/emacs.d/elisp"))
 	     (when (file-directory-p default-directory)
            (add-to-list 'load-path default-directory)
            (normal-top-level-add-subdirs-to-load-path)))
