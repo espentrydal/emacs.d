ccecdcbb3cf3cd19e69e7ca868f7361ee16a5047
Espen Trydal <espen@trydal.io>
2022-04-20 13:02:00 +0200

    org screenshot

--- config.el
+++ config.el
@@ -337,20 +337,37 @@
   :after org)
 (use-package citar)
 
-(use-package org-attach-screenshot
-  :after org
-  :bind ("C-c s" . org-attach-screenshot)
-  :config (setq org-attach-screenshot-dirfunction
-        (lambda ()
-          (progn (cl-assert (buffer-file-name))
-             (concat (file-name-sans-extension (buffer-file-name))
-                 "-att")))
-        org-attach-screenshot-command-line "gnome-screenshot -a -f %f"))
-(use-package org-download
-  :straight (:host github :repo "abo-abo/org-download")
-  :after org
-  :custom (setq org-download-image-dir "~/org/assets/images")
-  :bind ("C-c w . org-download-clipboard"))
+(cond ((string-match-p "\\`PC" (system-name))
+;;        (defun my-org-screenshot ()
+;;          "Take a screenshot into a time stamped unique-named file in the
+;; same directory as the org-buffer and insert a link to this file."
+;;          (interactive)
+;;          (setq filename
+;;                (concat
+;;                 (make-temp-name
+;;                  (concat (buffer-file-name)
+;;                          "_"
+;;                          (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+;;          (shell-command "snippingtool /clip")
+;;          (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+;;          (insert (concat "[[file:" filename "]]"))
+;;          (org-display-inline-images))
+       )
+      (t
+       (use-package org-attach-screenshot
+         :after org
+         :bind ("C-c s" . org-attach-screenshot)
+         :config (setq org-attach-screenshot-dirfunction
+                       (lambda ()
+                         (progn (cl-assert (buffer-file-name))
+                                (concat (file-name-sans-extension (buffer-file-name))
+                                        "-att")))
+                       org-attach-screenshot-command-line "gnome-screenshot -a -f %f"))
+       (use-package org-download
+         :straight (:host github :repo "abo-abo/org-download")
+         :after org
+         :custom (setq org-download-image-dir "~/org/assets/images")
+         :bind ("C-c w" . org-download-clipboard))))
 
 ;; Org reveal
 (use-package ox-reveal
