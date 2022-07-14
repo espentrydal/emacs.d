73ceddd91bdf3940e71fe0c7f0c1706daf5ce5bd
Espen Trydal <espen@trydal.io>
2022-07-14 13:48:37 +0200

    org agenda

--- custom.el
+++ custom.el
@@ -7,6 +7,8 @@
  '(conda-anaconda-home (expand-file-name "~/miniconda3/"))
  '(custom-safe-themes
    '("82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default))
+ '(org-agenda-files
+   '("/home/espen/02-org/beorg/inbox.org" "/home/espen/02-org/beorg/datamaskin.org" "/home/espen/02-org/beorg/husarbeid.org"))
  '(safe-local-variable-values '((conda-project-env-path . "pytorch")))
  '(sml/theme 'automatic)
  '(tool-bar-mode nil)
